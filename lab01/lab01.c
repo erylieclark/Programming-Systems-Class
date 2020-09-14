5f45c2eb895ba8466a5aecabf1a9bf6cf06a11df
erylieclark <erylieclark@gmail.com>
2020-09-14 15:08:00 -0700

    Initial commit for lab01.

---
+++ lab01/lab01.c
@@ -0,0 +1,7 @@
+/*Include the standard input/output library*/
+#include <stdio.h>
+
+int main(){
+    /* Write hello world to the console when the file is executed*/
+    printf("Hello, World!\n");
+}
