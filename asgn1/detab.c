043dbe8996f874b35c027759d9dc90767d823b64
erylieclark <erylieclark@gmail.com>
2020-09-17 18:23:59 -0700

    "Potentially ready to do initial tests to check for bugs"

--- asgn1/detab.c
+++ asgn1/detab.c
@@ -1,7 +1,30 @@
+/*****************************************************************************
+* Filename: main.c
+*
+* Description: This program will take in a file and turn any tabs in the file
+* into the appropriate number of spaces.
+*
+* Author: Erin Rylie Clark
+*
+* Revisions:
+*
+******************************************************************************/
+
 #include <stdio.h>
 
 #define TAB 8
 
+/*------------------------------------------------------------------------------
+* Function: tab
+*
+* Description:
+*
+*
+* Param 1:
+* Param 2:
+*
+* return: 
+*-----------------------------------------------------------------------------*/
 int tab(int tSet, int tCount){
     int nSpc = TAB-(tCount % tSet);
     for (i=0;i<nSpc;i++)
@@ -18,7 +41,7 @@ int main(){
 
     int tCount = 0; /* Variable for tracking the spaces */
     int tSet = 0; /* Variable for tracking how many sets of 8 have been used */
-
+    int lCount = 0; /* Count the number of lines in the file */
     while ((c = getchar()) != EOF){
         switch(c){
             case '\n':
@@ -30,6 +53,8 @@ int main(){
                 tSet = tCount/TAB;
                 break;
             case '\b':
+                tCount--;
+                tSet = tCount/TAB;
                 // Some if else to count possible decrements
                 break;
             case '\r': /* Do we care about return carriage? */
@@ -40,8 +65,5 @@ int main(){
                 tSet = tCount/TAB;
                 break;
         }
-        putchar(); // Maybe
     }
-
-    putchar(); // Maybe
 }
