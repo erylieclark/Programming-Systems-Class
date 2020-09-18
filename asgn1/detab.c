3a1a65044c4c9dfd64b152fb749f4045dd2b9d15
erylieclark <erylieclark@gmail.com>
2020-09-18 16:44:03 -0700

    Added return carriage functionality and finalized comments and README file.

--- asgn1/detab.c
+++ asgn1/detab.c
@@ -2,77 +2,83 @@
 * Filename: main.c
 *
 * Description: This program will take in a file and turn any tabs in the file
-* into the appropriate number of spaces.
+* into the appropriate number of spaces. It will additionally account for any
+* special characters including newline, backspace, and return carriage.
 *
 * Author: Erin Rylie Clark
-*
-* Revisions:
 *
 ******************************************************************************/
 
 #include <stdio.h>
 
-#define TAB 8
+#define TAB 8 /* A tab is defined as 8 spaces */
 
 /*------------------------------------------------------------------------------
 * Function: tab
 *
-* Description:
+* Description: This function takes in the column count and the number of tab
+* sets, and outputs the correct number of spaces when the tab key is detected.
+* It will return the resulting column count to the main function.
+* 
+* Param 1: tSet - how many sets of 8 spaces currently in tCount (starts at 0)
+* Param 2: tCount - current column count
 *
-*
-* Param 1:
-* Param 2:
-*
-* return: 
+* return: tCount - updated column count after tab key is detected
 *-----------------------------------------------------------------------------*/
 int tab(int tSet, int tCount){
-    int nSpc = TAB-(tCount - TAB*tSet);
+
+    int nSpc = TAB-(tCount - TAB*tSet);     /* Number of spaces to next tab
+                                                stop */ 
     int i = 0;
     
+        /* Output the appropriate number of spaces. Skip if none are needed. */
     while(i<nSpc){
         putchar(' ');
         i++;
     }
     
-    tCount += nSpc;
+    tCount += nSpc;     /* New tCount is prev tCount + spaces added */
     return tCount;
 }
 
-void back(int tCount){
-    tCount--;
-}
+/*------------------------------------------------------------------------------
+* Function: main
+*-----------------------------------------------------------------------------*/
 
 void main(){
 
-    int tCount = 0; /* Variable for tracking the spaces */
-    int tSet = 0; /* Variable for tracking how many sets of 8 have been used */
-    int lCount = 0; /* Count the number of lines in the file */
-    int c;
-    while ((c = getchar()) != EOF){
+    int tCount = 0;     /* Variable for tracking the current column */
+    int tSet = 0;       /* Sets of 8 in the current column count */
+    int c;              /* Store the current character */
+    while ((c = getchar()) != EOF){ /* Loop until end of file */
         switch(c){
             case '\n':
-                tCount = 0; /* Reset tCount and tSet with a new line */
+                tCount = 0;         /* Newline resets Count and Sets */
                 tSet = tCount/TAB;
                 break;
             case '\t':
-                tCount = tab(tSet, tCount);
-                tSet = tCount/TAB;
-                continue;
+                tCount = tab(tSet, tCount); /* Call tab function for '\t'*/
+                tSet = tCount/TAB;  /* Recalc set after function call */
+                continue;           /* tab function takes care of output, so
+                                        skip to next loop without hitting
+                                        putchar() at bottom */
             case '\b':
-                if (tCount > 0)
+                if (tCount > 0)     /* Check if tCount will go negative */
                     tCount--;
                 else
-                    tCount = 0;
-                tSet = tCount/TAB;
-                /* Some if else to count possible decrements */
+                    tCount = 0;     /* Keep tCount at 0 if already 0 */
+                tSet = tCount/TAB;  /* Recalc tSet based on new tCount */
                 break;
-            case '\r': /* Do we care about return carriage? */
+            case '\r':
+                tCount = 0;         /* '\r' sets the column back to beginning
+                                        of line, so reset tCount */
+                tSet = 0;
                 break;
-            default: /* Default will be any other characters*/
-                tCount++;
+            default:                /* Default will be any other characters*/
+                tCount++;           /* Increment tCount and recalc tSet */
                 tSet = tCount/TAB;
                 break;
         }
-        putchar(c);
+        putchar(c); /* For all cases apart from tab, output the character */
     }
 }
