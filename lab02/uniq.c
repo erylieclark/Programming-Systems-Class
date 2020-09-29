12b69a73e45ff2ff82f289a350a54bd2376570a4
erylieclark <erylieclark@gmail.com>
2020-09-28 23:23:03 -0700

    Lab02 - Final upload with README and makefile

--- lab02/uniq.c
+++ lab02/uniq.c
@@ -1,8 +1,10 @@
 /*******************************************************************************
 * Filename: uniq.c
+* 
 * Description: This file is inteneded to take a file input, and remove any
 *	lines that are a duplicate of the previous line. File size and line
 *	length should not be limited.*
+* 
 * Author: Erin Rylie Clark
 *******************************************************************************/
 #include <stdio.h>
@@ -12,34 +14,48 @@ FILE *stdin;
 FILE *stdout;
 /*------------------------------------------------------------------------------
 * Function name: read_long_line
+* 
 * Description: This function will take in the file, and read the next line. It
-* 	will allocate new space for the long line and then free the space when
-*	it is done with the information.
+* 	will allocate new space for the line, write the line into that buffer, and 
+*   return a pointer to the beginning of the buffer.
+* 
+* param: FILE *file, a pointer called file to a FILE object given by *stdin
+* 
+* return: char *, a pointer to a character. This will contain NULL if the EOF
+*   has been detected, or will return a pointer to the first character of 
+*   the current line.
 *-----------------------------------------------------------------------------*/
 char *read_long_line(FILE *file){
-    /* Figure out how long this line is */
-    int len = 0;
-    static int file_pos = 0;
-    char c, *mbuffer;
-    fseek(file, file_pos, SEEK_SET);
-    c = fgetc(file);
-    while( c != '\n' && c != EOF ){ 
-	/* Determine length of line, stopped by '\n' character*/
-        len++;
-        c = fgetc(file);
+    int len = 0; /* Length of current line */
+    static int file_pos = 0; /* # of bytes(or chars) that have been accounted
+                                for in the file. Static in order to keep place
+                                of next line in between function calls */
+    char c, *mbuffer; /* c to store cur char, mbuffer to store pntr to line */
+    fseek(file, file_pos, SEEK_SET); /* Adjust file pointer to the correct spot
+                                        in the file from last call. If first
+                                        call, no adjustment will take place. */
+    c = fgetc(file); /* Get the first character for the while loop to check */
+    while( c != '\n' && c != EOF ){ /* Determine length of the line, do not
+                                        inc len or get next char if curr char
+                                        is '\n' or EOF  */
+        len++;              /* Increase length of line if not '\n' or EOF */
+        c = fgetc(file);    /* Get next character for while loop to check */
     }
-    if ( c == EOF ){
+    if ( c == EOF ){        /* If EOF, return a NULL pointer to main */
         return NULL;
     }
     else if ( c == '\n' ){
-        fseek(file, (-len-1), SEEK_CUR);
-        /* Allocate the necessary amount of space using malloc */
-        mbuffer = malloc( len + 1 );
-        /* Write the line into the buffer */
-        mbuffer = fgets(mbuffer, len+1, file);
-        /* Return the pointer to the buffer	*/
-        file_pos += len +1;
-        return mbuffer;
+        len++;              /* Account for '\n' character at end of line */
+        mbuffer = malloc( len );        /* Allocate a memory block long enough
+                                            for the line */
+        fseek(file, (-len), SEEK_CUR);  /* Return file pntr to beginning of
+                                            line in order to write line to
+                                            mbuffer using fgets */
+        mbuffer = fgets(mbuffer, len, file); /* Write the line into mbuffer and
+                                                return the pointer to mbuffer */
+        file_pos += len;    /* Store the cur file pntr position in the static
+                                variable to save for the next function call */
+        return mbuffer;     /* Return pntr to line to main for comparison */
     }
     else {
         exit(EXIT_FAILURE);
@@ -50,69 +66,36 @@ char *read_long_line(FILE *file){
 * Function: main
 *-----------------------------------------------------------------------------*/
 int main(){
-    char *line_1, *line_2, *lnptr;
-    int first_line;
-    first_line = 1;
-    /* Call to read long line */
-    while(1){
+    char *line_1, *line_2, *lnptr; /* 2 lines for compare, 1 storage pointer */
+    int first_line = 1             /* Indicate if on first line */
+
+    while(1){   /* Run this loop until reach EOF, indicated by NULL */
         
         if ( (lnptr = read_long_line(stdin)) == NULL ){
-            break;
+            break;      /* Retrieve the beginning of the next line in lnptr */
         }
-        else if(first_line){
-            line_1 = lnptr;
-            puts(line_1);
-            first_line = 0;
-            continue;
+        else if(first_line){    /* If this if first line in stdin file */
+            line_1 = lnptr;     /* Store the pointer in line1 (free() later) */
+            puts(line_1);       /* Print the line to stdout */
+            first_line = 0;     /* First line has been taken care of */
+            continue;           /* Skip comparison of line2 */
         }
         else{
-            line_2 = lnptr;
+            line_2 = lnptr;     /* If not first line in stdin file, always
+                                    store new pointer to line2 */
         }
 
-        if ( strcmp(line_1, line_2) == 0 ){
-            free(line_2);
+        if ( strcmp(line_1, line_2) == 0 ){ /* Compare line1 and line2 */
+            free(line_2);   /* If they are the same, free the space
+                                allocated to line2, and continue in next loop
+                                to retrieve the next line into line_2*/
         }
         else{
-            free(line_1);
-            puts(line_2);
-            line_1 = line_2;
+            free(line_1);    /* If the lines are different, free line1 as
+                                it is not needed for comparison anymore */
+            puts(line_2);    /* Print 2nd line to stdout */
+            line_1 = line_2; /* Line2 now stored in line1 for next comparison */
         }
     }   
     return 0;
 }
-
-    /* Store the first pointer for first line */
-    /* Print first line */
-
-    /* If EOF, do nothing */
-    /* Call RLL again */
-    /* Store second pointer for second line */
-    /* Compare lines */
-    /* If second line is different, print second line,
- *  and free buffer for first line */
-    /* If second line is same, do not print, free buffer for second line */
-
-
-
-/*------------------------------------------------------------------------------
-* Funtion name: safe_malloc
-* This code was mostly taken from lecture_05 notes
-*-----------------------------------------------------------------------------*/
-/*void safe_malloc(size_t size){
-    void *new;
-    if ( !(new=malloc(size)) ){
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    return new;
-} */
-/*------------------------------------------------------------------------------
-* Function name: print_line
-*-----------------------------------------------------------------------------*/
-/*void print_line(char* c){
-    while( *c != '\0'){
-        puts(c);
-        c++;
-    }
-}
-*/
